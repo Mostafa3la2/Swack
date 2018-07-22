c9361ff32f73500ce6aaea3254e6476479ef2190
Mostafa Alaa <mostafaassassin@gmail.com>
2018-07-23 00:15:52 +0200

    Creaed the login UI
    created the basic UI for the login VC, added only the close functionality for now.

---
+++ Swack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Swack
+//
+//  Created by Mostafa Alaa on 7/23/18.
+//  Copyright © 2018 Mostafa Alaa. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Swack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Swack
+//
+//  Created by Mostafa Alaa on 7/22/18.
+//  Copyright © 2018 Mostafa Alaa. All rights reserved.
+//
+
+import Foundation
+
+//segues
+
+let LOGIN = "loginSegue"
