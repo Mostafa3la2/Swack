3547215da5efa206ca9d02d65c605d00326e3e9f
Mostafa Alaa <mostafaassassin@gmail.com>
2018-07-26 16:43:14 +0200

    Added findAllChannels request

---
+++ Swack/Model/Channel.swift
@@ -0,0 +1,14 @@
+//
+//  Channel.swift
+//  Swack
+//
+//  Created by Mostafa Alaa on 7/26/18.
+//  Copyright © 2018 Mostafa Alaa. All rights reserved.
+//
+
+import Foundation
+struct Channel{
+    private(set) public var channelid:String!
+    private(set) public var channedDescription:String!
+    private(set) public var channelTitle:String!
+}
---
+++ Swack/Services/MessageService.swift
@@ -0,0 +1,47 @@
+//
+//  MessageService.swift
+//  Swack
+//
+//  Created by Mostafa Alaa on 7/26/18.
+//  Copyright © 2018 Mostafa Alaa. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+class MessageService{
+    
+    static let instance = MessageService()
+    private init(){
+        
+    }
+    var channels = [Channel]()
+    
+    
+    func findAllChannels(completion:@escaping CompletionHandler ){
+        Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+            if response.result.error == nil{
+                guard let data = response.data else{return}
+                do{
+                if let json = try JSON(data:data).array{
+                    for item in json{
+                        let name = item["name"].stringValue
+                        let channenDescription = item["description"].stringValue
+                        let id = item["_id"].stringValue
+                        let channel = Channel(channelid: id, channedDescription: channenDescription, channelTitle: name)
+                        self.channels.append(channel)
+                        
+                    }
+                }
+                }catch let error as NSError{
+                    debugPrint(error)
+                }
+                completion(true)
+            }else{
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+    
+}
